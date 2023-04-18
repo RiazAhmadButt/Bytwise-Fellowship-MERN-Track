63ff50106b1715eea22a1081bcc0f24286a57ff3
Riaz Ahmad Butt <70515400+RiazAhmadButt@users.noreply.github.com>
2023-04-18 13:56:22 +0500

    Add files via upload

---
+++ JavaScript Projects/Quote Generator/app.js
@@ -0,0 +1,59 @@
+// variables 
+
+const btn = document.querySelector('#new-quote');
+const text = document.querySelector('p');
+const person = document.querySelector('span');
+
+// JSON Data  
+const quotes = [
+    {
+        "quote": "The only sin is ignorance",
+        "source": "Christopher Marlowe"
+    },
+    {
+        "quote": "A man is his own easiest dupe, for what he wishes to be true he generally believes to be true",
+        "source": "Demosthenes"
+    },
+    {
+        "quote": "A lie can travel halfway around the world while the truth is putting on its shoes",
+        "source": "Mark Twain"
+    },
+    {
+        "quote": "The only sin is ignorance",
+        "source": "Christopher Marlowe"
+    },
+    {
+        "quote": "A man is his own easiest dupe, for what he wishes to be true he generally believes to be true",
+        "source": "Demosthenes"
+    },
+    {
+        "quote": "A lie can travel halfway around the world while the truth is putting on its shoes",
+        "source": "Mark Twain"
+    },
+    {
+        "quote": "Great minds discuss ideas; average minds discuss events; small minds discuss people",
+        "source": "Eleanor Roosevelt"
+    },
+    {
+        "quote": "If you have a garden and a library, you have everything you need",
+        "source": "Marcus Tullius Cicero"
+    },
+    {
+        "quote": "Truth comes out in wine",
+        "source": "Pliny the Elder"
+    },
+    {
+        "quote": "Everything in the universe is within you. Ask all from yourself",
+        "source": "Rumi"
+    },
+    {
+        "quote": "When I get a little money I buy books; and if any is left I buy food and clothes",
+        "source": "Desiderius Erasmus"
+    }
+]
+
+btn.addEventListener('click', function () {
+    let random = Math.floor(Math.random() * quotes.length);
+    text.innerText = quotes[random].quote;
+    person.innerText = quotes[random].source;
+})
